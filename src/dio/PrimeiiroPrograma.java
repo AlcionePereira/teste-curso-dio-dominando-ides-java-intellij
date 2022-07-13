e5bef0dbff1a78dbd3c1c4de11c28d42f3a78f70
Alcione Pereira <catce.2021211mtds0453@gmail.com.ifpi.edu>
2022-07-13 18:20:17 -0300

    fisrt commit

---
+++ src/dio/PrimeiiroPrograma.java
@@ -0,0 +1,14 @@
+package dio;
+
+import dio.modeeeeel.Gato;
+
+public class PrimeiiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello wolrd! " + (a+b));*/
+    }
+}
